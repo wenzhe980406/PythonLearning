a4ab9fc7ce9e2f10ee2ee6b6b12f8d049a654ccb
wenzhe980406 <54115267+wenzhe980406@users.noreply.github.com>
2019-08-30 22:46:40 +0800

    Add files via upload

---
+++ day01/helloWorld.py
@@ -0,0 +1,11 @@
+# _*_ coding : UTF-8 _*_
+# 开发人员 : ChangYw
+# 开发时间 : 2019/7/15 11:16
+# 文件名称 : helloWorld.PY
+# 开发工具 : PyCharm
+
+print("hello world1")
+
+if __name__  == "__main__" :
+    print("hello world2")
+
---
+++ day01/变量赋值.py
@@ -0,0 +1,24 @@
+# _*_ coding : UTF-8 _*_
+# 开发人员 : ChangYw
+# 开发时间 : 2019/7/15 14:23
+# 文件名称 : 变量赋值.PY
+# 开发工具 : PyCharm
+
+counter = 100
+print(counter)
+
+counter = 50
+print(counter)
+
+name = 'hwua'
+print(name)
+
+print('name = ',name)
+
+a=b=c=d=10
+print(a,b,c,d)
+
+a,b,c,d =1,2,3,'hwua'
+print(a,b,c,d)
+del d
+print(d)
